e61d4ff7200a96f718b5e290b971769636183cb2
CLown1331 <clown1331@yahoo.com>
2019-01-27 21:19:28 +0600

    added

---
+++ 326.power-of-three.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=326 lang=cpp
+ *
+ * [326] Power of Three
+ *
+ * https://leetcode.com/problems/power-of-three/description/
+ *
+ * algorithms
+ * Easy (41.28%)
+ * Total Accepted:    164.7K
+ * Total Submissions: 398.8K
+ * Testcase Example:  '27'
+ *
+ * Given an integer, write a function to determine if it is a power of three.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 27
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 0
+ * Output: false
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 9
+ * Output: true
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: 45
+ * Output: false
+ * 
+ * Follow up:
+ * Could you do it without using any loop / recursion?
+ */
+class Solution {
+public:
+    bool isPowerOfThree(int num) {
+        if (num == 1) return true;
+        if (num == 0) return false;
+        if (num % 3) return false;
+        return isPowerOfThree(num / 3);
+    }
+};
