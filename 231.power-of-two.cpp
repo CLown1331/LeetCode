2d9add4bdfa285907d83a13a436d4c601665435e
CLown1331 <clown1331@yahoo.com>
2019-01-27 17:50:53 +0600

    added

---
+++ 231.power-of-two.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=231 lang=cpp
+ *
+ * [231] Power of Two
+ *
+ * https://leetcode.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (41.51%)
+ * Total Accepted:    207.9K
+ * Total Submissions: 500.8K
+ * Testcase Example:  '1'
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: true 
+ * Explanation: 20 = 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 218
+ * Output: false
+ * 
+ */
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return (n > 0 && __builtin_popcount(n) == 1);
+    }
+};
